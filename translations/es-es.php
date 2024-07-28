67c7c3c902b955f59befdc3d51bed3d2efd4e722
Douglas Martins <douglas5422@outlook.com>
2024-07-28 20:39:07 -0300

    Atualizando as outras linguagens

--- translations/es-es.php
+++ translations/es-es.php
@@ -3,34 +3,35 @@
 define("WELCOME_TO_API", "Bienvenido a la API");
 define("SAVED_SITE_SETTINGS", "Configuraciones del sitio guardadas con éxito.");
 define("SAVED_WEBSITE_INFORMATION", "Información del sitio guardada con éxito.");
-define("UPDATED_IMAGES", "Imágenes actualizadas con éxito");
+define("UPDATED_IMAGES", "Imágenes actualizadas con éxito.");
+define("SAVE_SETTINGS", "Configuraciones guardadas con éxito.");
 
-// Usuarios
-define("USER_CREATED", "Su cuenta ha sido creada con éxito.");
+// Users
+define("USER_CREATED", "Tu cuenta ha sido creada con éxito.");
 define("INVALID_CREDENTIALS", "Usuario o contraseña inválidos.");
-define("SESSION_EXPIRED", "Su sesión ha expirado, por favor inicie sesión nuevamente.");
-define("NOT_LOGGED_IN", "Por favor inicie sesión para acceder a esta área.");
+define("SESSION_EXPIRED", "Tu sesión ha expirado, inicia sesión de nuevo.");
+define("NOT_LOGGED_IN", "Por favor, inicia sesión para acceder a esta área.");
 define("USERS", "Usuarios");
 define("LABEL_PERMISSIONS_USERS", USERS);
 define("EMAIL_IN_USE", "Este correo electrónico ya está en uso.");
 define("NO_PERMISSION_TO_ACCESS", "No tienes permiso para acceder a esta área.");
-define("TEXT_MESSAGE_ACCOUNT", "Esta cuenta es solo para fines de prueba, no tiene permiso para editar, crear o eliminar contenidos.");
+define("TEXT_MESSAGE_ACCOUNT", "Esta cuenta es solo para fines de prueba, no tiene permiso para editar, crear o eliminar contenido.");
 define("NOT_ALLOWED", "No tienes permiso para realizar esta acción.");
 
-// Errores
-define("FATAL_ERROR", "Ups, ocurrió un error, por favor intente de nuevo.");
+// Errors
+define("FATAL_ERROR", "Ops, ocurrió un error, inténtalo de nuevo.");
 
-// Validadores
+// Validators
 define("UNSUPPORTED_IMG", "El archivo debe ser una imagen JPG, JPEG o PNG.");
-define("LARGE_FILE", "El archivo no debe exceder 5MB.");
-define("UPLOAD_ERROR", "Error durante la subida del archivo.");
+define("LARGE_FILE", "El archivo no puede exceder los 5MB.");
+define("UPLOAD_ERROR", "Error durante la carga del archivo.");
 define("UNSUPPORTED_ICO", "El archivo debe ser un ícono ICO o JPG, JPEG o PNG.");
-define("INVALID_EMAIL" , "El correo electrónico es inválido.");
-define("INVALID_FIELD_ERROR", "El campo (%s) es inválido.");
-define("FIELD_INVALID_CHARACTERS", "El campo (%s) contiene caracteres inválidos.");
+define("INVALID_EMAIL", "El correo electrónico no es válido.");
+define("INVALID_FIELD_ERROR", "El campo (%s) no es válido.");
+define("FIELD_INVALID_CHARACTERS", "El campo (%s) contiene caracteres no válidos.");
 define("REQUIRED_FIELD", "El campo (%s) es obligatorio.");
 
-// Etiquetas de formulario
+// Form Labels
 define("SITE_NAME_LABEL", "Nombre del sitio");
 define("EMAIL_LABEL", "Correo electrónico");
 define("PHONE_LABEL", "Teléfono");
@@ -44,3 +45,22 @@ define("KEYWORDS_LABEL", "Palabras clave");
 define("NAME_LABEL", "Nombre");
 define("LAST_NAME_LABEL", "Apellido");
 define("PASSWORD_LABEL", "Contraseña");
+define("SERVER", "Servidor");
+define("EMAIL_ADDRESS", "Dirección de correo electrónico");
+define("USER_NAME", "Nombre de usuario");
+
+// Email Templates
+// WELCOME
+define("WELCOME_TITLE", "Bienvenido a");
+define("HIGHLIGHT_MESSAGE", "✨ ¡Descubre oportunidades increíbles para promover tu negocio! ✨");
+define("GREETING", "Hola");
+define("WELCOME_MESSAGE", "¡Felicidades por registrarte en nuestra Guía! Estamos emocionados de tenerte con nosotros y ofrecerte una plataforma increíble para promocionar y descubrir productos y servicios.");
+define("QUOTE_MESSAGE", "¡Explora las diversas oportunidades de promoción y conéctate con una audiencia ansiosa por descubrir novedades!");
+define("FEATURES_TITLE", "A continuación, algunos de los recursos que encontrarás en nuestra Guía:");
+define("FEATURE_1", "Publicación fácil y rápida de tus anuncios");
+define("FEATURE_2", "Comunidad comprometida de anunciantes y consumidores");
+define("FEATURE_3", "Promociones especiales y oportunidades destacadas");
+define("FEATURE_4", "Reseñas y comentarios valiosos de la comunidad");
+define("FEATURE_5", "Gestión eficiente de tus campañas publicitarias");
+define("HELP_MESSAGE", "¡Estamos aquí para ayudarte a obtener el máximo impacto con tus anuncios!");
+define("SIGNATURE", "Atentamente, <br> Equipo de");
--- translations/it-it.php
+++ translations/it-it.php
@@ -3,36 +3,37 @@
 define("WELCOME_TO_API", "Benvenuto nell'API");
 define("SAVED_SITE_SETTINGS", "Impostazioni del sito salvate con successo.");
 define("SAVED_WEBSITE_INFORMATION", "Informazioni sul sito salvate con successo.");
-define("UPDATED_IMAGES", "Immagini aggiornate con successo");
+define("UPDATED_IMAGES", "Immagini aggiornate con successo.");
+define("SAVE_SETTINGS", "Impostazioni salvate con successo.");
 
-// Utenti
+// Users
 define("USER_CREATED", "Il tuo account è stato creato con successo.");
 define("INVALID_CREDENTIALS", "Nome utente o password non validi.");
-define("SESSION_EXPIRED", "La tua sessione è scaduta, per favore accedi di nuovo.");
-define("NOT_LOGGED_IN", "Per favore accedi per accedere a questa area.");
+define("SESSION_EXPIRED", "La tua sessione è scaduta, effettua nuovamente il login.");
+define("NOT_LOGGED_IN", "Per favore, accedi per accedere a quest'area.");
 define("USERS", "Utenti");
 define("LABEL_PERMISSIONS_USERS", USERS);
-define("EMAIL_IN_USE", "Questo indirizzo email è già in uso.");
-define("NO_PERMISSION_TO_ACCESS", "Non hai il permesso per accedere a quest'area.");
-define("TEXT_MESSAGE_ACCOUNT", "Questo account è solo a scopo di test, non ha il permesso di modificare, creare o eliminare contenuti.");
-define("NOT_ALLOWED", "Non hai il permesso per eseguire questa azione.");
+define("EMAIL_IN_USE", "Questa e-mail è già in uso.");
+define("NO_PERMISSION_TO_ACCESS", "Non hai il permesso di accedere a quest'area.");
+define("TEXT_MESSAGE_ACCOUNT", "Questo account è solo per scopi di test, non ha il permesso di modificare, creare o eliminare contenuti.");
+define("NOT_ALLOWED", "Non hai il permesso di eseguire quest'azione.");
 
-// Errori
-define("FATAL_ERROR", "Oops, si è verificato un errore, per favore riprova.");
+// Errors
+define("FATAL_ERROR", "Ops, si è verificato un errore, riprova.");
 
-// Validatori
+// Validators
 define("UNSUPPORTED_IMG", "Il file deve essere un'immagine JPG, JPEG o PNG.");
-define("LARGE_FILE", "Il file non deve superare i 5MB.");
+define("LARGE_FILE", "Il file non può superare i 5MB.");
 define("UPLOAD_ERROR", "Errore durante il caricamento del file.");
 define("UNSUPPORTED_ICO", "Il file deve essere un'icona ICO o JPG, JPEG o PNG.");
-define("INVALID_EMAIL" , "L'email non è valida.");
+define("INVALID_EMAIL", "L'indirizzo e-mail non è valido.");
 define("INVALID_FIELD_ERROR", "Il campo (%s) non è valido.");
 define("FIELD_INVALID_CHARACTERS", "Il campo (%s) contiene caratteri non validi.");
 define("REQUIRED_FIELD", "Il campo (%s) è obbligatorio.");
 
-// Etichette del modulo
+// Form Labels
 define("SITE_NAME_LABEL", "Nome del sito");
-define("EMAIL_LABEL", "Email");
+define("EMAIL_LABEL", "E-mail");
 define("PHONE_LABEL", "Telefono");
 define("CITY_LABEL", "Città");
 define("STATE_LABEL", "Stato");
@@ -44,3 +45,22 @@ define("KEYWORDS_LABEL", "Parole chiave");
 define("NAME_LABEL", "Nome");
 define("LAST_NAME_LABEL", "Cognome");
 define("PASSWORD_LABEL", "Password");
+define("SERVER", "Server");
+define("EMAIL_ADDRESS", "Indirizzo e-mail");
+define("USER_NAME", "Nome utente");
+
+// Email Templates
+// WELCOME
+define("WELCOME_TITLE", "Benvenuto su");
+define("HIGHLIGHT_MESSAGE", "✨ Scopri opportunità incredibili per promuovere la tua attività! ✨");
+define("GREETING", "Ciao");
+define("WELCOME_MESSAGE", "Congratulazioni per esserti registrato al nostro Guida. Siamo entusiasti di averti con noi e offrirti una piattaforma straordinaria per promuovere e scoprire prodotti e servizi!");
+define("QUOTE_MESSAGE", "Esplora le numerose opportunità di promozione e collegati con un pubblico desideroso di scoprire novità!");
+define("FEATURES_TITLE", "Scopri di seguito alcune delle funzionalità che troverai nella nostra Guida:");
+define("FEATURE_1", "Pubblicazione facile e veloce dei tuoi annunci");
+define("FEATURE_2", "Comunità coinvolta di inserzionisti e consumatori");
+define("FEATURE_3", "Promozioni speciali e opportunità di visibilità");
+define("FEATURE_4", "Recensioni e feedback preziosi dalla comunità");
+define("FEATURE_5", "Gestione efficiente delle tue campagne pubblicitarie");
+define("HELP_MESSAGE", "Siamo qui per aiutarti a ottenere il massimo impatto con le tue pubblicità!");
+define("SIGNATURE", "Cordiali saluti, <br> Team di");
