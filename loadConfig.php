b86ae7e9c51e5e90875a6a30a30bde18c7260510
Douglas Martins <douglas5422@outlook.com>
2024-08-11 20:47:48 -0300

    feat: melhorias no arquivo de inicialização inicial

--- loadConfig.php
+++ loadConfig.php
@@ -2,89 +2,92 @@
 
 use App\Http\Response;
 
-loadTranslations();
-date_default_timezone_set(loadTimeZone());
-
-// Verifica se o script está sendo executado via CLI
-function isCli() {
-    return php_sapi_name() === 'cli' || defined('STDIN');
-}
-
-if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/favicon.ico') {
+if (isFaviconRequest()) {
     exit;
 }
 
-// Carrega as configurações de produção ou desenvolvimento
-$envPath = __DIR__ . "/";
-$envFile = '.env';
+// Load the appropriate .env file
+$envFile = determineEnvFile();
+loadEnv($envFile);
 
-if (file_exists($envPath . '.env.development')) {
-    define("DEV_MODE", true);
-    $envFile = '.env.development';
-} elseif (file_exists($envPath . '.env')) {
-    define("DEV_MODE", false);
-    $envFile = '.env';
-}
+// Configure CORS permissions
+handleCors();
 
-$dotenv = Dotenv\Dotenv::createImmutable($envPath, $envFile);
-$dotenv->load();
+// Set the default language
+loadTranslations();
 
-// Carrega as configurações de CORS
-$allowed_origin = $_ENV['ALLOWED_ORIGIN'];
-$requestOrigin = $_SERVER['HTTP_ORIGIN'] ?? '';
+// Sets the default time zone
+loadTimeZone();
 
-if (!isCli() && DEV_MODE) {
-    setHeaders($allowed_origin);
-
-} else if (!isCli() && !DEV_MODE) {
-    if($requestOrigin !== $allowed_origin) {
-        Response::json([
-            "error" => true,
-            "message" => "You are not authorized to access this API"
-        ], 403);
-
-        exit;
-    }
-
-    setHeaders($allowed_origin);
-}
-
-function setHeaders($allowed_origin) {
-    header("Access-Control-Allow-Origin: $allowed_origin");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, token");
-
-    if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header("HTTP/1.1 200 OK");
-        exit;
-    }
+function isFaviconRequest() {
+    return isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/favicon.ico';
 }
 
 function loadTranslations() {
     $rootPath = realpath(__DIR__);
-    $settings = loadFileSettings();
 
-    if (!isset($settings['language'])) {
-        throw new Exception("The language setting is not set.");
-    }
+    $language = getSetting('language') ?? "pt-br";
+    define("LANGUAGE", $language);
 
-    $language = $settings['language'];
     $translationFile = $rootPath . "/translations/$language.php";
 
     if (!file_exists($translationFile)) {
         throw new Exception("The translation file for language $language was not found.");
     }
 
-    define("LANGUAGE", $language);
-    include_once $translationFile;
+    include_once  $translationFile;
 }
 
 function loadTimeZone() {
-    $settings = loadFileSettings();
+    $timeZone = getSetting('timezone') ?? "America/Sao_Paulo";
+    date_default_timezone_set($timeZone);
+}
 
-    if (!isset($settings['timezone'])) {
-        return "America/Sao_Paulo";
+function determineEnvFile() {
+    $envPath = __DIR__ . "/";
+
+    if (file_exists($envPath . '.env.development')) {
+        define("DEV_MODE", true);
+        return '.env.development';
     }
 
-    return $settings['timezone'];
+    define("DEV_MODE", false);
+    return '.env';
+}
+
+function loadEnv($envFile) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, $envFile);
+    $dotenv->load();
+}
+
+function handleCors() {
+    $allowed_origin = $_ENV['ALLOWED_ORIGIN'] ?? '';
+    $requestOrigin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+    if (!isCli()) {
+        if (DEV_MODE || $requestOrigin === $allowed_origin) {
+            setCorsHeaders($allowed_origin);
+        } else {
+            Response::json([
+                "message" => "You are not authorized to access this API"
+            ], 403);
+            
+            exit;
+        }
+    }
+}
+
+function setCorsHeaders($allowed_origin) {
+    header("Access-Control-Allow-Origin: $allowed_origin");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, token");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("HTTP/1.1 200 OK");
+        exit;
+    }
+}
+
+function isCli() {
+    return php_sapi_name() === 'cli' || defined('STDIN');
 }
