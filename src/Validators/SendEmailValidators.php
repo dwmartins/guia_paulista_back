3a98f7b92655c105b25896effef7576770771338
Douglas Martins <douglas5422@outlook.com>
2024-08-11 21:03:45 -0300

    fix: Trando erros na valida

--- src/Validators/SendEmailValidators.php
+++ src/Validators/SendEmailValidators.php
@@ -8,56 +8,65 @@ use Exception;
 
 class SendEmailValidators {
     public static function contact(array $data) {
-        $fields = [
-            NAME_LABEL       => $data['name'] ?? '',
-            LAST_NAME_LABEL  => $data['lastName'] ?? '',
-            EMAIL_LABEL      => $data['email'] ?? '',
-            MESSAGE_LABEL    => $data['message'] ?? ''
-        ];
-
-        Validator::validate($fields);
-
-        $fields[COMPANY_LABEL] = $data['company'];
-
-        foreach ($fields as $key => $value) {
-            if(empty($value)) {
-                continue;
-            }
-
-            if($key === EMAIL_LABEL) {
-                if(!TextValidator::email($value)) {
-                    Response::json([
-                        'message'   => INVALID_EMAIL
-                    ], 400);
-
-                    return false;
+        try {
+            $fields = [
+                NAME_LABEL       => $data['name'] ?? '',
+                LAST_NAME_LABEL  => $data['lastName'] ?? '',
+                EMAIL_LABEL      => $data['email'] ?? '',
+                MESSAGE_LABEL    => $data['message'] ?? ''
+            ];
+    
+            Validator::validate($fields);
+    
+            $fields[COMPANY_LABEL] = $data['company'];
+    
+            foreach ($fields as $key => $value) {
+                if(empty($value)) {
+                    continue;
                 }
-
-                continue;
-            }
-
-            if($key === NAME_LABEL || $key === LAST_NAME_LABEL) {
-                if(!TextValidator::name($value)) {
+    
+                if($key === EMAIL_LABEL) {
+                    if(!TextValidator::email($value)) {
+                        Response::json([
+                            'message'   => INVALID_EMAIL
+                        ], 400);
+    
+                        return false;
+                    }
+    
+                    continue;
+                }
+    
+                if($key === NAME_LABEL || $key === LAST_NAME_LABEL) {
+                    if(!TextValidator::name($value)) {
+                        Response::json([
+                            'message'   => sprintf(FIELD_INVALID_CHARACTERS, $key)
+                        ], 400);
+        
+                        return false;
+                    }
+        
+                    continue;
+                }
+    
+                if(!TextValidator::text($value)) {
                     Response::json([
+                        'error'     => true,
                         'message'   => sprintf(FIELD_INVALID_CHARACTERS, $key)
                     ], 400);
-    
+        
                     return false;
                 }
-    
-                continue;
             }
+    
+            return true;
+            
+        } catch (Exception $e) {
+            Response::json([
+                'message'   => $e->getMessage()
+            ], 400);
 
-            if(!TextValidator::text($value)) {
-                Response::json([
-                    'error'     => true,
-                    'message'   => sprintf(FIELD_INVALID_CHARACTERS, $key)
-                ], 400);
-    
-                return false;
-            }
+            return false;
         }
-
-        return true;
     }
 }
